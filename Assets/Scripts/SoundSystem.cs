f93838980edc8c8fc51525e7000f81ca641e532d
Jonathan Thomson <jonathan.thomson.45@gmail.com>
2022-04-06 14:11:22 -0700

    Update options dialog to be able to configure BGM

--- Assets/Scripts/SoundSystem.cs
+++ Assets/Scripts/SoundSystem.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class SoundSystem : MonoBehaviour
@@ -25,8 +27,75 @@ public class SoundSystem : MonoBehaviour
     [SerializeField]
     public GameObject soundSystem;
 
-    private void Start() {
-        
+    [SerializeField]
+    public TMP_Dropdown bgmSelector;
+
+    private AudioSource theGridTheme;
+    private AudioSource ringOfGalaxyTheme;
+    private AudioSource suzakuCastleTheme;
+    private AudioSource airForceBaseTheme;
+    private AudioSource fangTheme;
+    private AudioSource karinTheme;
+    private AudioSource nashTheme;
+
+    private AudioSource currentlyPlayingTheme;
+
+    void Start() {
+        List<TMP_Dropdown.OptionData> opts = new List<TMP_Dropdown.OptionData>() {
+            new TMP_Dropdown.OptionData("The Grid"),
+            new TMP_Dropdown.OptionData("Ring Of Galaxy"),
+            new TMP_Dropdown.OptionData("Suzaku Castle"),
+            new TMP_Dropdown.OptionData("AirForce Base"),
+            new TMP_Dropdown.OptionData("FANG Theme"),
+            new TMP_Dropdown.OptionData("Karin Theme"),
+            new TMP_Dropdown.OptionData("Nash Theme"),
+        };
+        bgmSelector.AddOptions(opts);
+        bgmSelector.onValueChanged.AddListener(OnDropdownSelect);
+
+        GameObject BGM = soundSystem.transform.Find("BGM").gameObject;
+        theGridTheme = BGM.transform.Find("TheGridTheme").gameObject.GetComponent<AudioSource>();
+        ringOfGalaxyTheme = BGM.transform.Find("RingOfGalaxyTheme").gameObject.GetComponent<AudioSource>();
+        suzakuCastleTheme = BGM.transform.Find("SuzakuCastleTheme").gameObject.GetComponent<AudioSource>();
+        airForceBaseTheme = BGM.transform.Find("AirForceBaseTheme").gameObject.GetComponent<AudioSource>();
+        fangTheme = BGM.transform.Find("FangTheme").gameObject.GetComponent<AudioSource>();
+        karinTheme = BGM.transform.Find("KarinTheme").gameObject.GetComponent<AudioSource>();
+        nashTheme = BGM.transform.Find("NashTheme").gameObject.GetComponent<AudioSource>();
+
+        currentlyPlayingTheme = theGridTheme;
+        theGridTheme.Play();
+    }
+
+    private void OnDropdownSelect(int e) {
+        switch (e) {
+            case 0:
+                PlayTheme(theGridTheme);
+                break;
+            case 1:
+                PlayTheme(ringOfGalaxyTheme);
+                break;
+            case 2:
+                PlayTheme(suzakuCastleTheme);
+                break;
+            case 3:
+                PlayTheme(airForceBaseTheme);
+                break;
+            case 4:
+                PlayTheme(fangTheme);
+                break;
+            case 5:
+                PlayTheme(karinTheme);
+                break;
+            case 6:
+                PlayTheme(nashTheme);
+                break;
+        }
+    }
+
+    private void PlayTheme(AudioSource newTheme) {
+        currentlyPlayingTheme.Stop();
+        newTheme.Play();
+        currentlyPlayingTheme = newTheme;
     }
 
     public void PlayNormalAttackHit() {
