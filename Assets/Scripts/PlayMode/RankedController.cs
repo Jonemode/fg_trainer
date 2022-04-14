4ef8131a8df0f470b329aaba29aa5159002ad633
Jonathan Thomson <jonathan.thomson.45@gmail.com>
2022-04-13 21:34:57 -0700

    Add league text on uprank and downrank

--- Assets/Scripts/PlayMode/RankedController.cs
+++ Assets/Scripts/PlayMode/RankedController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RankedController : MonoBehaviour
 {
@@ -12,7 +13,21 @@ public class RankedController : MonoBehaviour
     public StateController stateController;
 
     [SerializeField]
+    public TMP_Dropdown simDropdown;
+
+    [SerializeField]
+    public Toggle stunBarToggle;
+
+    [SerializeField]
     public TMP_Text LpText;
+
+    [SerializeField]
+    public GameObject leagueContainer;
+
+    private GameObject leagueUpText;
+    private GameObject leagueDownText;
+    private GameObject upArrow;
+    private GameObject downArrow;
 
     private const string playerLpPrefsKey = "player_lp";
     
@@ -31,6 +46,11 @@ public class RankedController : MonoBehaviour
             playerRanks.Add(child.gameObject);
         }
 
+        leagueUpText = leagueContainer.transform.Find("LeagueUpText").gameObject;
+        leagueDownText = leagueContainer.transform.Find("LeagueDownText").gameObject;
+        upArrow = leagueContainer.transform.Find("UpArrow").gameObject;
+        downArrow = leagueContainer.transform.Find("DownArrow").gameObject;
+
         displayLp = PlayerPrefs.GetInt(playerLpPrefsKey);
         targetLp = displayLp;
         currentRank = GetRank();
@@ -44,7 +64,11 @@ public class RankedController : MonoBehaviour
             UpdateLpText();
         }
         if (displayLp > targetLp) {
-            displayLp -= 1;
+            if (displayLp - targetLp > 20) {
+                displayLp -= 3;
+            } else {
+                displayLp -= 1;
+            }            
             UpdateLpText();
         }
     }
@@ -52,14 +76,17 @@ public class RankedController : MonoBehaviour
     public void EnableRankedMode() {
         playerRankContainer.SetActive(true);
         LpText.gameObject.SetActive(true);
+        leagueContainer.SetActive(true);
         stateController.PlayerError += onPlayerError;
         stateController.HitConfirm += onConfirm;
         stateController.BlockConfirm += onConfirm;
+        ConfigureGame();
     }
 
     public void DisableRankedMode() {
         playerRankContainer.SetActive(false);
         LpText.gameObject.SetActive(false);
+        leagueContainer.SetActive(false);
         stateController.PlayerError -= onPlayerError;
         stateController.HitConfirm -= onConfirm;
         stateController.BlockConfirm -= onConfirm;
@@ -70,7 +97,7 @@ public class RankedController : MonoBehaviour
     }
 
     private void onPlayerError(object sender, EventArgs e) {
-        targetLp -= (25 + (10 * (int)currentRank));
+        targetLp -= (110 + (10 * (int)currentRank));
         if (targetLp < 0) {
             targetLp = 0;
         }
@@ -80,20 +107,49 @@ public class RankedController : MonoBehaviour
             playerRanks[(int)r + 1].SetActive(false);
             playerRanks[(int)r].SetActive(true);
             currentRank = r;
+            ConfigureGame();
+            ShowLeagueDown();
+        } else {
+            HideLeagueMessage();
         }
         PlayerPrefs.SetInt(playerLpPrefsKey, targetLp);
     }
 
     private void onConfirm(object sender, EventArgs e) {
-        targetLp += 75 - (3 * (int)currentRank);
+        targetLp += 100 - (5 * (int)currentRank);
         PlayerRank r = GetRank();
         if (r != currentRank) {
             // rank up
             playerRanks[(int)r - 1].SetActive(false);
             playerRanks[(int)r].SetActive(true);
             currentRank = r;
+            ConfigureGame();
+            ShowLeagueUp();
+        } else {
+            HideLeagueMessage();
         }
         PlayerPrefs.SetInt(playerLpPrefsKey, targetLp);
+    }
+
+    private void ShowLeagueUp() {
+        leagueUpText.SetActive(true);
+        upArrow.SetActive(true);
+        leagueDownText.SetActive(false);
+        downArrow.SetActive(false);
+    }
+
+    private void ShowLeagueDown() {
+        leagueDownText.SetActive(true);
+        downArrow.SetActive(true);
+        leagueUpText.SetActive(false);
+        upArrow.SetActive(false);
+    }
+
+    private void HideLeagueMessage() {
+        leagueUpText.SetActive(false);
+        leagueDownText.SetActive(false);
+        upArrow.SetActive(false);
+        downArrow.SetActive(false);
     }
 
     private PlayerRank GetRank() {
@@ -137,6 +193,49 @@ public class RankedController : MonoBehaviour
             return PlayerRank.UltimateGrandMaster;
         } else {
             return PlayerRank.Warlord;
+        }
+    }
+
+    private void ConfigureGame() {
+        switch (currentRank) {
+            case PlayerRank.Rookie:
+                simDropdown.value = (int)SimMode.QuarterSpeed;
+                stunBarToggle.isOn = true;
+                break;
+            case PlayerRank.Bronze:
+                simDropdown.value = (int)SimMode.HalfSpeed;
+                stunBarToggle.isOn = true;
+                break;
+            case PlayerRank.SuperBronze:
+            case PlayerRank.UltraBronze:
+                simDropdown.value = (int)SimMode.HalfSpeed;
+                stunBarToggle.isOn = true;
+                break;
+            case PlayerRank.Silver:
+            case PlayerRank.SuperSilver:
+            case PlayerRank.UltraSilver:
+            case PlayerRank.Gold:
+            case PlayerRank.SuperGold:
+                simDropdown.value = (int)SimMode.PC;
+                stunBarToggle.isOn = true;
+                break;
+            case PlayerRank.UltraGold:
+            case PlayerRank.Platinum:
+            case PlayerRank.SuperPlatinum:
+            case PlayerRank.UltraPlatinum:
+            case PlayerRank.Diamond:
+            case PlayerRank.SuperDiamond:
+            case PlayerRank.UltraDiamond:
+            case PlayerRank.Master:
+            case PlayerRank.GrandMaster:
+            case PlayerRank.UltimateGrandMaster:
+                simDropdown.value = (int)SimMode.PC;
+                stunBarToggle.isOn = false;
+                break;
+            case PlayerRank.Warlord:
+                simDropdown.value = (int)SimMode.PS4;
+                stunBarToggle.isOn = false;
+                break;
         }
     }
 }
